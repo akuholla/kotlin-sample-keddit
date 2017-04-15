83436dd355ed6284734a23c9a490ce64b04e5379
Akshay Holla <aholla@usertesting.com>
2017-04-15 08:36:16 -0700

    Include view type to model

--- app/src/main/java/com/akshayholla/keddit/commons/models/RedditNewsItem.kt
+++ app/src/main/java/com/akshayholla/keddit/commons/models/RedditNewsItem.kt
@@ -1,8 +1,13 @@
 package com.akshayholla.keddit.commons.models
 
+import com.akshayholla.keddit.commons.adapter.AdapterConstants
+import com.akshayholla.keddit.commons.adapter.ViewType
+
 data class RedditNewsItem(val author: String,
                           val title: String,
                           val numComment: Int,
                           val created: Long,
                           val thumbnail: String,
-                          val url: String)
+                          val url: String) : ViewType {
+                                                override fun getViewType() = AdapterConstants.NEWS
+}
