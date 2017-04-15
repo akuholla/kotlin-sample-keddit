63e62f2406471f08b557f778a63ed4626487c1b0
Akshay Holla <aholla@usertesting.com>
2017-04-15 12:18:07 -0700

    Makes model parcelable

--- app/src/main/java/com/akshayholla/keddit/commons/models/RedditNewsItem.kt
+++ app/src/main/java/com/akshayholla/keddit/commons/models/RedditNewsItem.kt
@@ -1,18 +1,69 @@
 package com.akshayholla.keddit.commons.models
 
+import android.os.Parcel
+import android.os.Parcelable
 import com.akshayholla.keddit.commons.adapter.AdapterConstants
 import com.akshayholla.keddit.commons.adapter.ViewType
+import com.akshayholla.keddit.commons.extensions.createParcel
 
 data class RedditNews(
         val after: String,
         val before: String,
-        val news: List<RedditNewsItem>)
+        val news: List<RedditNewsItem>) : Parcelable {
+
+    companion object {
+        @JvmField @Suppress("unused")
+        val CREATOR = createParcel { RedditNews(it) }
+    }
+
+    protected constructor(parcelIn: Parcel): this(
+            parcelIn.readString(),
+            parcelIn.readString(),
+            mutableListOf<RedditNewsItem>().apply {
+                parcelIn.readTypedList(this, RedditNewsItem.CREATOR)
+            }
+    )
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(after)
+        dest.writeString(before)
+        dest.writeTypedList(news)
+    }
+
+    override fun describeContents() = 0
+}
 
 data class RedditNewsItem(val author: String,
                           val title: String,
                           val numComment: Int,
                           val created: Long,
                           val thumbnail: String,
-                          val url: String) : ViewType {
-                                                override fun getViewType() = AdapterConstants.NEWS
+                          val url: String) : ViewType,Parcelable {
+
+    companion object {
+        @JvmField @Suppress("unused")
+        val CREATOR = createParcel { RedditNewsItem(it) }
+    }
+
+    protected constructor(parcelIn: Parcel) : this (
+            parcelIn.readString(),
+            parcelIn.readString(),
+            parcelIn.readInt(),
+            parcelIn.readLong(),
+            parcelIn.readString(),
+            parcelIn.readString()
+    )
+
+    override fun writeToParcel(dest: Parcel, flags: Int) {
+        dest.writeString(author)
+        dest.writeString(title)
+        dest.writeInt(numComment)
+        dest.writeLong(created)
+        dest.writeString(thumbnail)
+        dest.writeString(url)
+    }
+
+    override fun describeContents() = 0
+
+    override fun getViewType() = AdapterConstants.NEWS
 }
