0e6240c2e3a096930042360e9c50c47ecc735c94
Akshay Holla <aholla@usertesting.com>
2017-04-15 11:09:58 -0700

    Adds news manager that provides observables

---
+++ app/src/main/java/com/akshayholla/keddit/features/news/NewsManager.kt
@@ -0,0 +1,29 @@
+package com.akshayholla.keddit.features.news
+
+import com.akshayholla.keddit.api.RestAPI
+import com.akshayholla.keddit.commons.models.RedditNewsItem
+import rx.Observable
+
+class NewsManager(private val api: RestAPI = RestAPI()) {
+
+    fun getNews(limit: String = "10"): Observable<List<RedditNewsItem>> {
+        return Observable.create {
+            subscriber ->
+
+            val callResponse = api.getNews("", limit)
+            val response = callResponse.execute()
+
+            if (response.isSuccessful) {
+                val news = response.body().data.children.map {
+                    val item = it.data
+                    RedditNewsItem(item.author, item.title, item.num_comments,
+                            item.created, item.thumbnail, item.url)
+                }
+                subscriber.onNext(news)
+                subscriber.onCompleted()
+            } else {
+                subscriber.onError(Throwable(response.message()))
+            }
+        }
+    }
+}
