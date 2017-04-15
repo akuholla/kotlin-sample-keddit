cff4995fec7c44fac87f5d8bfcade1b23c0c79a6
Akshay Holla <aholla@usertesting.com>
2017-04-15 12:34:34 -0700

    Fragment uses parcelable

--- app/src/main/java/com/akshayholla/keddit/features/news/NewsFragment.kt
+++ app/src/main/java/com/akshayholla/keddit/features/news/NewsFragment.kt
@@ -18,6 +18,10 @@ import rx.schedulers.Schedulers
 
 class NewsFragment : RxBaseFragment() {
 
+    companion object {
+        private val KEY_REDDIT_NEWS = "redditNews"
+    }
+
     private var redditNews: RedditNews? = null
     private val newsManager: NewsManager by lazy { NewsManager() }
 
@@ -29,15 +33,20 @@ class NewsFragment : RxBaseFragment() {
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
 
-        news_list.setHasFixedSize(true)
-        val linearLayout = LinearLayoutManager(context)
-        news_list.layoutManager = linearLayout
-        news_list.clearOnScrollListeners()
-        news_list.addOnScrollListener(InfiniteScrollListener( { requestNews() }, linearLayout))
+        news_list.apply {
+            setHasFixedSize(true)
+            val linearLayout = LinearLayoutManager(context)
+            layoutManager = linearLayout
+            clearOnScrollListeners()
+            addOnScrollListener(InfiniteScrollListener( { requestNews() }, linearLayout))
+        }
 
         initAdapter()
 
-        if(savedInstanceState == null) {
+        if(savedInstanceState != null && savedInstanceState.containsKey(KEY_REDDIT_NEWS)) {
+            redditNews = savedInstanceState.get(KEY_REDDIT_NEWS) as RedditNews
+            (news_list.adapter as NewsAdapter).clearAndAddNews(redditNews!!.news)
+        } else {
             requestNews()
         }
     }
@@ -57,6 +66,14 @@ class NewsFragment : RxBaseFragment() {
                         }
                 )
         subscriptions.add(subscription)
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        val news = (news_list.adapter as NewsAdapter).getNews()
+        if (redditNews != null && news.size > 0) {
+            outState.putParcelable(KEY_REDDIT_NEWS, redditNews?.copy(news = news))
+        }
     }
 
     private fun initAdapter() {
