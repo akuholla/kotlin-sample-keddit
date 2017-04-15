8add4977c033fa3bdfa5cb73b86953137746a0ed
Akshay Holla <aholla@usertesting.com>
2017-04-15 10:47:17 -0700

    Api model

---
+++ app/src/main/java/com/akshayholla/keddit/api/ApiModels.kt
@@ -0,0 +1,20 @@
+package com.akshayholla.keddit.api
+
+class RedditNewsResponse(val data: RedditDataRespone)
+
+class RedditDataRespone(
+        val children: List<RedditChildrenResponse>,
+        val after: String?,
+        val before: String?
+)
+
+class RedditChildrenResponse(val data: RedditNewsDataResponse)
+
+class RedditNewsDataResponse(
+        val author: String,
+        val title: String,
+        val num_comments: Int,
+        val create: Long,
+        val thumbnail: String,
+        val url: String
+)
